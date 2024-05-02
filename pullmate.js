c817d0edc7204e8c8c02dc37a033b290366f410d
VikashAnandJha <mailtovikashjha@gmail.com>
2024-05-03 02:57:49 +0530

    test

--- pullmate.js
+++ pullmate.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Function to pull changes from the repository
 function pullChanges() {
-    exec('git pull origin main', { cwd: path.join(__dirname, 'testrepox') }, (error, stdout, stderr) => {
+    exec('git pull', { cwd: __dirname }, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error pulling changes: ${error.message}`);
             return;
@@ -18,8 +18,8 @@ function pullChanges() {
 
 // Schedule pulling changes every 5 seconds
 setInterval(() => {
-    console.log('Checking for changes...');
+    console.log('Checking for changes...11');
     pullChanges();
-}, 2000);
+}, 3000);
 
 console.log('Script running. Press Ctrl+C to exit.');
